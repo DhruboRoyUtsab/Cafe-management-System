21f75f3657dbd379a8b435e1c9bbd7fb1444052c
Bishal Paul <k.bishal.paul@gmail.com>
2026-01-17 02:17:45 +0600

    Waiter Sign in

---
+++ CMS/WaiterSignIn.cs
@@ -0,0 +1,39 @@
+﻿using CMS.Admin;
+using CMS.Assets;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace CMS
+{
+    public partial class WaiterSignIn : Form
+    {
+        public WaiterSignIn()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        private void BckBtn_Click(object sender, EventArgs e)
+        {
+            Homepage hm = new Homepage();
+            hm.Show();
+            this.Hide();
+        }
+
+        private void BtnWaiterSIgnIn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
